ce11e94a3a509679e5df983fc7618b31211ca74d
Ronaldo Cipriano <ronaldolopes14@gmail.com>
2024-05-09 15:36:54 -0300

    Iniciando projeto e criando um calendário dinamico

--- script.js
+++ script.js
@@ -14,7 +14,25 @@ const createDaysOfTheWeek = () => {
 
 createDaysOfTheWeek();
 
-const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const createCalendar = () => {
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysList = document.getElementById('days');
 
-// Escreva seu código abaixo.
+  decemberDaysList.forEach(day => {
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = day;
+    dayListItem.classList.add('day');
 
+    if ([24, 25, 31].includes(day)) {
+      dayListItem.classList.add('holiday');
+    }
+
+    if ([4, 11, 18, 25].includes(day)) {
+      dayListItem.classList.add('friday');
+    }
+
+    daysList.appendChild(dayListItem);
+  });
+}
+
+createCalendar();
